b33fc216f285ef09914a2c363c3dadab71c7ff9b
nabilaba <nabilazizbimaanggita@gmail.com>
2022-07-13 19:39:03 +0700

    added modal when click 'masuk'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { Routes, Route } from "react-router-dom";
 import Homepage from "./router/Homepage";
 
 function App() {
-  return <Homepage />;
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/router/Homepage/index.jsx
+++ src/router/Homepage/index.jsx
@@ -1,7 +1,23 @@
-import { Flex, Box, Stack, Heading, Button, chakra } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  Stack,
+  Heading,
+  Button,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  ModalFooter,
+  Text,
+} from "@chakra-ui/react";
 import background from "../../assets/image/background.jpg";
 
 export default function Homepage() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Box
       w="full"
@@ -25,11 +41,33 @@ export default function Homepage() {
           >
             Quiz With Trivia
           </Heading>
-          <Button colorScheme="blue" textTransform="uppercase" w="fit-content">
+          <Button
+            colorScheme="blue"
+            textTransform="uppercase"
+            w="fit-content"
+            onClick={onOpen}
+          >
             Masuk
           </Button>
         </Stack>
       </Flex>
+      <Modal onClose={onClose} size={"full"} isOpen={isOpen}>
+        <ModalOverlay />
+        <ModalContent m={10} borderRadius={10}>
+          <ModalHeader>MASUK</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt
+              incidunt est doloremque minus sapiente maxime odio, quisquam
+              doloremque veritatis quam tempore quisquam, quidem quisquam!
+            </Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>Close</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
