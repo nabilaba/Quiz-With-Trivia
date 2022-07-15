df6f4d3eaf64004671f39f3de6e7a84c1e790fc3
nabilaba <nabilazizbimaanggita@gmail.com>
2022-07-15 13:05:10 +0700

    update tampilan depan kuis

--- src/router/Dashboard/Kuis/index.jsx
+++ src/router/Dashboard/Kuis/index.jsx
@@ -2,8 +2,20 @@ import useGlobalState from "../../../globalstate";
 import { useEffect } from "react";
 import API from "../../../constant";
 import { Link as LinkTo } from "react-router-dom";
-import { Button, Container, Heading } from "@chakra-ui/react";
+import {
+  Button,
+  Container,
+  Heading,
+  Text,
+  Image,
+  OrderedList,
+  ListItem,
+  Stack,
+  Box,
+} from "@chakra-ui/react";
 import { Helmet } from "react-helmet";
+import contohkuis1 from "../../../assets/image/contohkuis1.png";
+import contohkuis2 from "../../../assets/image/contohkuis2.png";
 export default function Kuis() {
   const { namaKuis, setDataKuis, setIdSoal, dataKuis } = useGlobalState();
   useEffect(() => {
@@ -17,26 +29,56 @@ export default function Kuis() {
 
   console.log(dataKuis);
   return (
-    <Container maxW="full" p={4}>
+    <Container maxW="full">
       <Helmet>
         <title>QWT - Kuis {namaKuis}</title>
       </Helmet>
-      <Heading fontSize="3xl" fontWeight="bold">
-        {namaKuis}
-      </Heading>
-      <Button
-        as={LinkTo}
-        to="mengerjakan"
-        color="white"
-        bg="accent.50"
-        _hover={{ bg: "accent.100" }}
-        _active={{ bg: "accent.100" }}
-        onClick={() => {
-          setIdSoal(0);
-        }}
-      >
-        Mulai
-      </Button>
+      <Stack spacing={0}>
+        <Text fontSize="xl" fontWeight="bold">
+          ✍️ Cara mengerjakan Kuis
+        </Text>
+        <OrderedList stylePosition={"inside"}>
+          <ListItem>Baca kuis dengan teliti</ListItem>
+          <ListItem>Pilih jawaban yang menurutmu benar</ListItem>
+          <ListItem>
+            Kamu bisa menekan tombol "Selanjutnya" untuk melanjutkan
+          </ListItem>
+          <ListItem>
+            Kamu juga bisa menekan tombol "Sebelumnya" untuk kembali ke kuis
+            sebelumnya
+          </ListItem>
+          <ListItem>
+            Jika jawaban yang kamu pilih semua menurutmu benar, di akhir kuis
+            kamu bisa menekan Selesai
+          </ListItem>
+          <ListItem>Perhatikan juga waktu yang tersisa</ListItem>
+        </OrderedList>
+      </Stack>
+      <Stack spacing={2} mt={2}>
+        <Text fontSize="xl" fontWeight="bold">
+          📷 Contoh - Contoh Kuis
+        </Text>
+        <Image src={contohkuis1} alt="contoh kuis" />
+        <Image src={contohkuis2} alt="contoh kuis" />
+      </Stack>
+      <Box position="sticky" bottom={0} w="full">
+        <Button
+          as={LinkTo}
+          to="mengerjakan"
+          color="white"
+          bg="accent.50"
+          _hover={{ bg: "accent.100" }}
+          _active={{ bg: "accent.100" }}
+          onClick={() => {
+            setIdSoal(0);
+          }}
+          my={4}
+          w="full"
+          textTransform="uppercase"
+        >
+          Mulai dengan sekali ketuk
+        </Button>
+      </Box>
     </Container>
   );
 }
